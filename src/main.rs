65ec53b12cd5905140d047a3e10ab0e61b76ceba
TheFinnishArmy <thefinnisharmy@gmail.com>
2019-10-16 09:13:56 +0000

    Added quota parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-extern crate serde;
 extern crate reqwest;
 
-use serde::{Serialize, Deserialize};
 use reqwest::Error;
 
 fn main() -> Result<(), Error> {
-    println!("Hello, world!");
 
     let quota_url = String::from("https://www.random.org/quota/?format=plain");
 
     let mut quota_response = reqwest::get(&quota_url)?;
+
+    let mut quota_text = quota_response.text()?;
+´
+    let quota = quota_text.parse::<i32>().unwrap();
+
+    let quota = match f {
+        Ok(i32) => i32,
+        Err(error) => {
+            panic!("Could not get quota successfully");
+        }
+    }
+    
+
+    println!("{}", quota_text);
 
     let request_url = format!("https://www.random.org/sequences/?min={min}&max={max}&col={colums}&format={format}&rnd={randomMethod}",
                     min = 1,
@@ -20,8 +31,6 @@ fn main() -> Result<(), Error> {
     
     );
 
-    println!("{}", request_url);
-
     let mut response = reqwest::get(&request_url)?;
 
     let mut cards : Vec<i8>;
@@ -30,7 +39,7 @@ fn main() -> Result<(), Error> {
 
 
 
-    println!("\n{:?}", response_text);
+    println!("\n{}", response_text);
 
     Ok(())
 }
