2c4d2d8c0dc3e9fc3f14d81421e52065298e510b
TheFinnishArmy <thefinnisharmy@gmail.com>
2019-10-17 06:50:36 +0000

    Change over to randomness from rust itself

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,30 @@
-extern crate reqwest;
 extern crate rand;
 
-use reqwest::Error;
-use reqwest::header::USER_AGENT;
 use rand::prelude::*;
 
-fn main() -> Result<(), Error> {
-    let client = reqwest::Client::new();
-
-    //Hardcoded quota string to check if we still have bits of randomness we can use 
-    let quota_url = String::from("https://www.random.org/quota/?format=plain");
-
-    let mut quota_response = client.get(&quota_url)
-        .header(USER_AGENT, "tRustyProbability - thefinnisharmy@gmail.com")
-        .send()?;
-
-    //Move into it's own string object, and remove leading and trailing whitespace, so that parsing doesn't fail
-    let mut quota_text = quota_response.text()?;
-    quota_text = quota_text.trim().to_string();
-
-    //Try to parse to an int, if fails something else is wrong and we should exec further so we panic
-    let mut quota = quota_text.parse::<i32>().unwrap();
-    
-    //println!("{}", quota_text);
-
-    if quota < 1613 {panic!("Not enough bits of randomness available for randomizing deck");};
+fn main() {
 
     let mut total_draws : i32 = 0;
     let mut finished_rounds : i32 = 0;
 
-    while quota > 1613 {
-        //Create url for the https request, and then send it
-        let request_url = format!("https://www.random.org/sequences/?min={min}&max={max}&col={colums}&format={format}&rnd={randomMethod}",
-                    min = 1, //Minium value to be returned, for a card deck should be 1
-                    max = 52, //Maximum value to be returned, for a card deck should be 52
-                    colums = 1,
-                    format = "plain", //Either plain for plaintext, or html for html formated normal page. Plain is easier to code for.
-                    randomMethod = "today"     //Options are, "new" for using new bits, or date to use pregenerated randomness for that day
-                    );
+    let mut iterations = 1000;
 
-        let mut response = client.get(&request_url)
-            .header(USER_AGENT, "tRustyProbability - thefinnisharmy@gmail.com")
-            .send()?;
+    while iterations > 0 {
+        iterations -= 1;
 
-        //Calculate still available quota, to check whether we can do anoter round
-        quota = quota - 1612;
+        //Initialize vector
+        let mut cards : Vec<i8> = (1..53).collect();
 
-        //Move response body into its own variable
-        let cards_text : String = response.text()?;
-
-        //Initialize vectors
-        let mut split_card_text : Vec<&str> = Vec::with_capacity(52);
-        let mut cards : Vec<i8> = Vec::with_capacity(52);
-
-        //Split the body into discrete "numbers"
-        let mut cards_text_vector = cards_text.split_whitespace();
-
-        //Iterate over those strings pushing them into a vector, while also converting them into an interger
-        for i in 0..52 {
-            split_card_text.push(cards_text_vector.next().unwrap());
-            cards.push(split_card_text[i].parse::<i8>().unwrap());
-        }
-
-        //Init rng for shuffling all aces cards back into the deck, worse rng than random.org, but using it here for saving bits on the requests 
-        //(Plus makes handling still available bits easier)
+        //Init rng for shuffiling the first order of the deck, and for shuffling aces back into the deck
         let mut rng = thread_rng();
+
+        //Shuffle the original opsition of the deck
+        cards.shuffle(&mut rng);
         
         //Loops variables to track their names
         let mut draws : i32 = 0;
         let mut found_aces : i32 = 0;
-        
+
         //"Drawing" one value from the deck continuously until we reach 4 aces cards
         while found_aces < 4 {
             draws += 1;
@@ -89,7 +44,7 @@ fn main() -> Result<(), Error> {
 
         //Printing the statistics out to the console
         println!("This rounds draws: {}", draws);
-        println!("Still available random bits: {} \n \n", quota);
+        println!("Rounds still left to go: {}\n", iterations);
     };
 
     println!("Overall we did {} rounds of drawing.", finished_rounds);
@@ -102,7 +57,4 @@ fn main() -> Result<(), Error> {
     let average_number_of_draws : f64 = total_draws_float / finished_rounds_float;
 
     println!("So on average you need to draw {} cards to get 4 aces cards with this setup.", average_number_of_draws);
-
-    //Returning Ok, is here so that error handling otherwhere is easier
-    Ok(())
 }
